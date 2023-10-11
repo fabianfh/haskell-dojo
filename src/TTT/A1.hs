1e9e09cff04c530140ba487d8c53242c132e14dd
Fabian Hoefer <fabian_hoefer@web.de>
2023-10-11 11:14:22 +0200

    Assignment A1

--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -4,48 +4,84 @@ import Data.Char (toUpper)
 
 -- Q#01
 
-_SIZE_ = undefined
+_SIZE_:: Int
+_SIZE_ = 3
 
 -- Q#02
 
-_DISPLAY_LOGO_ = undefined
+_DISPLAY_LOGO_ :: Bool
+_DISPLAY_LOGO_ = True
 
 -- Q#03
 
-convertRowIndex = undefined
+convertRowIndex:: Char -> Int
+convertRowIndex c = fromEnum (toUpper c) - 65
 
 -- Q#04
 
-_INVALID_MOVE_ = undefined
+_INVALID_MOVE_ = (-1,-1)
 
 -- Q#05
 
-_SEP_ = undefined
+_SEP_ = "_|_"
 
 -- Q#06
 
-data Square
+data Square = X | O | Empty deriving (Show,Eq)
 
 -- Q#07
 
-data GameState
+data GameState = XWin | XLoose | Draw | InProg deriving (Show,Eq)
+
+
 
 -- Q#08
 
--- Q#09
+type Player =  Square
+type Row    = [Square]
+type Line   = [Square]
+type Board  = [Row]
+type Move   = (Int, Int)
 
-getFirstPlayer = undefined
 
-getFirstPlayer_ = undefined
+-- Q#09 = 
+
+getFirstPlayer b = if b then X else O
+
+getFirstPlayer_ b  
+       | b =  X
+       | otherwise  = O
 
 -- Q#10
+showGameState:: GameState -> String
+showGameState state = case state of 
+    XWin -> "X Wins"
+    XLoose -> "O Wins" 
 
-showGameState = undefined
+
+
 
 -- Q#11
+switchPlayer:: Player -> Player
+switchPlayer X = O 
+switchPlayer O = X 
+switchPlayer Empty = Empty 
 
-switchPlayer = undefined
+
+
 
 -- Q#12
+showSquare:: Square -> String
+showSquare X = "X"
+showSquare O = "O"
+showSquare Empty = "_"
 
-showSquare = undefined
+
+
+
+
+
+
+
+
+
