7be9626d9b48772d7cec2cc8cd673ef6e9f613f2
Fabian Hoefer <fabian_hoefer@web.de>
2023-10-27 16:09:55 +0200

    assignment2 v2

--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -18,7 +18,7 @@ convertRowIndex:: Char -> Int
 convertRowIndex c = fromEnum (toUpper c) - 65
 
 -- Q#04
-
+_INVALID_MOVE_:: (Int,Int)
 _INVALID_MOVE_ = (-1,-1)
 
 -- Q#05
--- src/TTT/A2.hs
+++ src/TTT/A2.hs
@@ -5,46 +5,87 @@ import TTT.A1
 
 -- Q#01
 
-promptPlayer = undefined
-
+promptPlayer:: Player -> IO () 
+promptPlayer pler =  print  $ concat ["Player ", show pler, " 's turn: enter a row and column position (ex. A1)"]
 -- Q#02
 
-_RANGE_ = undefined
+_RANGE_ = [1 .. _SIZE_]
 
 -- Q#03
 
-isDigit = undefined
+isDigit:: Char -> Bool
+isDigit c =elem c ['0' .. '9'] 
 
-readDigit = undefined
+readDigit:: Char -> Int
+readDigit c = if isDigit c then read [c] else -1
 
 -- Q#04
 
-_EMPTY_ROW_ = undefined
+_EMPTY_ROW_ :: Row
+_EMPTY_ROW_ = replicate _SIZE_ Empty
 
-_EMPTY_BOARD_ = undefined
+_EMPTY_BOARD_:: Board
+_EMPTY_BOARD_ = replicate _SIZE_ _EMPTY_ROW_
 
 -- Q#05
+isTied:: Board -> Bool
+isTied board = all isRowTied board
+    where
+        isRowTied:: Row -> Bool 
+        isRowTied row = all (/= Empty) row 
 
-isTied = undefined
+_TIED_BOARD_ :: Board
+_TIED_BOARD_ = [
+    [X, O, O]
+  , [O, X, X]
+  , [O, X, O]]
 
-_TIED_BOARD_ = undefined
 
 -- Q#06
 
-indexRowStrings = undefined
+
+indexRowStrings:: [String] -> [(Char,String)]
+indexRowStrings row  = zip ['A' ..] row 
 
 -- Q#07
 
-formatLine = undefined
+formatLine:: [String] -> String
+formatLine row = concat [_SEP_, (intercalate _SEP_ row), _SEP_]
 
 -- Q#08
 
-isMoveInBounds = undefined
+isMoveInBounds:: Move -> Bool
+isMoveInBounds (x,y)= and [x < _SIZE_, x>= 0,y < _SIZE_, y>= 0] 
 
 -- Q#09
 
-stringToMove = undefined
+stringToMove:: [Char] -> Move 
+stringToMove  [a,b] = if isMoveInBounds mv then mv else _INVALID_MOVE_
+                      where fst = (convertRowIndex a) 
+                            snd = (readDigit b) -1
+                            mv = (fst,snd)
+stringToMove _ = _INVALID_MOVE_
 
 -- Q#10
 
 replaceSquareInRow = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
